06fe084b264b26e946c98944f24a16110a32882e
nikitagordeev10 <nikitagordeev10@yandex.ru>
2024-10-24 23:54:09 +0300

    оформил для github

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LimitedSizeStack {
+
+    public class LimitedSizeStack<T> {
+
+        private T[] items; // массив items элементов типа T
+        private int top = 0; // позиция вершины
+        private int stackSize = 0; // размер стека
+
+        // Ограничение размера стека
+        public LimitedSizeStack(int undoLimit) {
+            // создаём массив items, размера undoLimit, сосотоящий из элементов типа T
+            items = new T[undoLimit];
+        }
+
+        // Добавить на вершину стека
+        public void Push(T item) {
+            // Если ограничение размера стека 0, то ничего не добавляем
+            if (items.Length == 0)
+                return;
+            // Иначе, добавить элемент на вершину стека (DropOutStack)
+            items[top] = item;
+            top = (top + 1) % items.Length;
+            // Если стек заполнен не до конца, то запомнить размер
+            if (stackSize < items.Length)
+                stackSize++;
+        }
+
+        // Удалить с вершины стека
+        public T Pop() {
+            // Если стек пуст, то выдаём ошибку
+            if (stackSize == 0)
+                throw new System.InvalidOperationException("Stack is empty");
+            // Иначе, удалить элемент с вершины стка (DropOutStack)
+            top = ((items.Length - 1) + top) % items.Length;
+            stackSize--;
+            return items[top];
+        }
+
+        // Текущий размер стека
+        public int Count {
+            get {
+                return stackSize;
+            }
+        }
+    }
+}
+
+/* Подсказки
+ * Для эффективной реализации такого стека подойдет двусвязный список. Он уже реализован в классе LinkedList
+ * LinkedList не реализует интерфейс IList, поэтому Last() для него работает медленно. Однако у него есть собственное свойство Last, работающее быстро.
+ */
+
+/* Материалы
+ * Могу ли я ограничить глубину общего стека? // stackovergo URL: https://stackovergo.com/ru/q/54316/can-i-limit-the-depth-of-a-generic-stack (дата обращения: 19.02.2023).
+ * DropOutStack // URL: https://courses.cs.vt.edu/~cs2704/spring04/projects/DropOutStack.pdf (дата обращения: 19.02.2023).
+ */
---
+++ ListModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack {
+    public class ListModel<TItem> {
+        public List<TItem> Items { get; }
+        public int UndoLimit;
+        private LimitedSizeStack<Action> undoActions;
+
+        public ListModel(int undoLimit) {  // Конструктор класса
+            Items = new List<TItem>();
+            UndoLimit = undoLimit;
+            undoActions = new LimitedSizeStack<Action>(undoLimit);
+        }
+
+        public ListModel(List<TItem> items, int undoLimit) { // Конструктор класса
+            Items = items;
+            UndoLimit = undoLimit;
+            undoActions = new LimitedSizeStack<Action>(undoLimit);
+        }
+
+        public void AddItem(TItem item) { // добавление нового элемента в список
+            Items.Add(item); // добавление элемента в список
+            AddUndoAction(new Action(RemoveItemFromList));
+        }
+
+        public void RemoveItem(int index) { // Удаление элемента из списка по индексу
+            var item = Items[index];
+            Items.RemoveAt(index);
+            Action undoAction = InsertItemAtIndex; // Создание действия для отмены удаления элемента
+            void InsertItemAtIndex() { // Локальная функция добавления элемента на прежнее место
+                Items.Insert(index, item);
+            }
+            AddUndoAction(undoAction); // Добавление действия отмены удаления элемента
+        }
+
+        public bool CanUndo() { // Проверка на возможность выполнения операции отмены
+            return undoActions.Count > 0;
+        }
+
+        public void Undo() { // Отмена последнего действия
+            if (CanUndo()) { 
+                var action = undoActions.Pop(); // Если в списке undoActions есть элемент
+                action.Invoke(); // выполняется последнее добавленное действие
+            }
+        }
+
+        private void AddUndoAction(Action action) { // Добавление действия отмены последнего действия в стек
+            undoActions.Push(action);
+        }
+
+        private void RemoveItemFromList() { // Удаление последнего элемента из списка
+            Items.RemoveAt(Items.Count - 1);
+        }
+    }
+}
